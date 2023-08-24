6c00974b1fd3cc1c766ea0d72778bf8a3547c2b0
Joel <jrazuri98@hotmail.com>
2023-08-24 20:26:26 -0300

    Endpoints User terminado

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -15,4 +15,26 @@ async def get_users(db: Session = Depends(get_db)):
     return await UserService.get_users(db)
 
 
+@user_router.get("(/{id:int}", response_model=UserId)
+async def get_user(id:int, db: Session = Depends(get_db)):
+    user_by_id = await UserService.get_user_by_id(db, id)
+    
+    if not user_by_id:
+        raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail="El usuario no se encuentra.")    
+    
+    return user_by_id 
 
+
+@user_router.post("/", response_model=UserId, status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserData, db: Session = Depends(get_db)):
+    check_name = await UserService.get_user_by_username(db, user.username)
+
+    if check_name:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="El nombre de usuario ya existe.")
+    
+    return await UserService.create_user(db, user)
+    
