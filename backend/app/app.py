d5bc23130562ff9a2311030840e7effe0be89f52
Joel <jrazuri98@hotmail.com>
2023-08-31 00:20:57 -0300

    Agregando permisos CORS

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from app.routers.user import user_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origin = [
+    "http://localhost:5173/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origin,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def hello():
