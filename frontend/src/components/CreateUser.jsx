e950dfe98287694642d780720c4601fcf659ffc8
Joel <jrazuri98@hotmail.com>
2023-08-31 00:55:36 -0300

    Componente CreateUser terminado

--- frontend/src/components/CreateUser.jsx
+++ frontend/src/components/CreateUser.jsx
@@ -1,11 +1,43 @@
+import { useState } from 'react'
+
 export default function CreateUser() {
+    const [ textName, setTextName] = useState("")
+    const [ textPassword, setTextPassword] = useState("")
+
+    function handleTextName(e) {
+        setTextName(e.target.value)
+    }
+
+    function handleTextPassword(e) {
+        setTextPassword(e.target.value)
+    }
+
+    function handleClickForm(e) {
+        e.preventDefault()
+
+        fetch(
+            "http://localhost:8000/api/users", {
+                method: "POST",
+                headers: {"Content-type" : "application/json"},
+                body: JSON.stringify({
+                    name: textName,
+                    password: textPassword
+                })
+            }
+        ).then (() => {
+            setTextName("")
+            setTextPassword("")
+        })
+    }
+    
+    
     return (
         <form>
             <label htmlFor="name">Name:</label>
-            <input id="name" type="text"/>
+            <input id="name" type="text" onChange={handleTextName} value={textName}/>
             <label htmlFor="passwrod">Password:</label>
-            <input id="password" type="password"/>
-            <input type="submit" value={CreateUser}/>
+            <input id="password" type="password" onChange={handleTextPassword} value={textPassword}/>
+            <input type="submit" value={CreateUser} onClick={handleClickForm}/>
         </form>
     )
 }
