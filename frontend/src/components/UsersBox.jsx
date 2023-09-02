9281a85b191c5c9450686d54f4b4193130f22339
Joel <jrazuri98@hotmail.com>
2023-09-01 21:23:12 -0300

    Corrigiendo errores

--- frontend/src/components/UsersBox.jsx
+++ frontend/src/components/UsersBox.jsx
@@ -2,9 +2,9 @@
 export default function UserBox({name, password, id}) {
     return (
         <article>
+            <h3>{id}</h3>
             <h3>{name}</h3>
             <p>{password}</p>
-            <p>{id}</p>
         </article>
     )
 }
